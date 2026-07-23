fc176b018cfabc1ce287a1720fd5a818fff12c44
gautreynicolas-create <gautreynicolas@gmail.com>
2026-07-23 19:53:14 +0200

    Create photos-data.js

---
+++ data/photos-data.js
@@ -0,0 +1,23 @@
+window.ALBUMS = {
+    sport: { title: "Sport", photos: [
+          "https://picsum.photos/seed/sport-1/900/1200","https://picsum.photos/seed/sport-2/900/1200",
+          "https://picsum.photos/seed/sport-3/900/1200","https://picsum.photos/seed/sport-4/900/1200",
+          "https://picsum.photos/seed/sport-5/900/1200","https://picsum.photos/seed/sport-6/900/1200",
+          "https://picsum.photos/seed/sport-7/900/1200"
+        ]},
+    concerts: { title: "Concerts & Festivals", photos: [
+          "https://picsum.photos/seed/concert-1/900/1200","https://picsum.photos/seed/concert-2/900/1200",
+          "https://picsum.photos/seed/concert-3/900/1200","https://picsum.photos/seed/concert-4/900/1200",
+          "https://picsum.photos/seed/concert-5/900/1200","https://picsum.photos/seed/concert-6/900/1200"
+        ]},
+    evenementiel: { title: "Evenementiel local", photos: [
+          "https://picsum.photos/seed/event-1/900/1200","https://picsum.photos/seed/event-2/900/1200",
+          "https://picsum.photos/seed/event-3/900/1200","https://picsum.photos/seed/event-4/900/1200",
+          "https://picsum.photos/seed/event-5/900/1200"
+        ]},
+    reseaux: { title: "Contenus Reseaux Sociaux", photos: [
+          "https://picsum.photos/seed/social-1/900/1200","https://picsum.photos/seed/social-2/900/1200",
+          "https://picsum.photos/seed/social-3/900/1200","https://picsum.photos/seed/social-4/900/1200",
+          "https://picsum.photos/seed/social-5/900/1200"
+        ]}
+};
